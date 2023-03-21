30debc755edacb17e80dd15cb9868c5fdd9c601f
Ye-eun-Kim <kimyeeun@gm.gist.ac.kr>
2023-03-22 03:59:12 +0900

    for adjusting hyperparameters in pv_forecast_model.py

--- weather_processing.py
+++ weather_processing.py
@@ -1,29 +1,22 @@
-# Process weather data with observation and forecast and PV data
-# data duration: 360days 210101~211231 (210618, 210930, 211222~211224 missing)
-
-# 50 features = 24 PV + 3 observation + 23 forecast
-#  PV generation each hour
-#  'DS', 'SL', 'SR'
-'''
-'TM_6', 'TM_9', 'TM_12', 'TM_15', 'TM_18', 'WS_6', 'WS_9', 'WS_12', 'WS_15', 'WS_18',
-'SK_6', 'SK_9', 'SK_12', 'SK_5', 'SK_18', 'PP_6', 'PP_9', 'PP_12', 'PP_15', 'PP_18',
-'PR_9', 'PR_15', 'PR_21
-'''
-
-
-# import libraries
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 
-# load files
+
 pv = pd.read_csv('./processed_data/pv/RISE_2021_PV_no_interpol.csv', index_col=0)
-forecast_weather = pd.read_csv('./processed_data/weather/forecast_weather.csv')  # 기온(°C), 풍속(m/s), 하늘상태(1,3,4), 강수확률(%), 강수량(mm)
+forecast_weather = pd.read_csv('./processed_data/weather/forecast_weather.csv')
 observation_weather = pd.read_csv('./preprocessed_data/weather_data/weather_observation.csv', index_col=2, encoding='cp949').drop(['지점', '지점명'], axis=1)
-observation_weather.columns = ['DS', 'SL', 'SR']  # (DS) Duration of Sunshine: 가조시간(hr) | (SL) SunLight 합계 일조시간(hr) | (SR) Solar Radiation 합계 일사량(MJ/m2)
+load = pd.read_csv('./processed_data/load/week_2021_load.csv', index_col=0)
+cal = pd.read_csv('./processed_data/2021_cal_flag.csv', index_col=0)
+
+observation_weather.columns = ['DS', 'SL', 'SR']  # (DS) Duration of Sunlight: 가조시간(hr) | (SL) SunLight 합계 일조시간(hr) | (SR) Solar Radiation 합계 일사량(MJ/m2)
+cal_used = cal[['miss&week']]
+
 
 
 # modify date(일시) string into integer to convert the value to tensor.
+# set '일시' as index of observation_weather
 observation_weather_len = len(observation_weather)
 observation_weather_date_list = observation_weather.index
 mdi_observation_weather_date_list = []
@@ -35,17 +28,51 @@ for i in range(observation_weather_len):
     mdi_observation_weather_date_list.append(mdi_date)
 
 
-# set date as index of weather data
+# set the modified date as index of observation_weather and forecast_weather    
 observation_weather.index = mdi_observation_weather_date_list
 forecast_weather.index = mdi_observation_weather_date_list
 
 
-# make combined dataframe
+# concat pv, observation_weather, forecast_weather
+comb = pd.concat([pv, observation_weather, forecast_weather], axis=1, join='outer')
 # 50 features = 24 PV + 3 observation + 23 forecast
-comb = pd.concat([pv, observation_weather, forecast_weather], axis=1, join='inner')
+#  PV generation each hour
+#  'DS', 'SL', 'SR'
+#  'TM_6', 'TM_9', 'TM_12', 'TM_15', 'TM_18', 'WS_6', 'WS_9', 'WS_12', 'WS_15', 'WS_18',
+#  'SK_6', 'SK_9', 'SK_12', 'SK_5', 'SK_18', 'PP_6', 'PP_9', 'PP_12', 'PP_15', 'PP_18',
+#  'PR_9', 'PR_15', 'PR_21'
 
-# There are NaN values in 210618, 210930
-comb = comb.drop([210618, 210930], axis=0)
+# sort the index of comb
+# after concat, the indices not in intersection are in the end of the dataframe
+comb.sort_index(inplace=True)
 
-# save the dataframe into file
-comb.to_csv('./processed_data/pv/2021_weather_and_RISEPV.csv')
+
+
+# set dataframes for pv and load
+X_pv = pd.DataFrame(columns=comb.columns)
+Y_pv = pd.DataFrame(columns=comb.columns)
+
+X_load = pd.DataFrame(columns=load.columns)
+Y_load = pd.DataFrame(columns=load.columns)
+
+
+# select the data of dates that are valid
+# valid: the day and the next day is valid
+cnt = 0
+for idx, row in comb.iterrows():
+    if cal_used.loc[idx]['miss&week'] == 0:
+        if cal_used.iloc[cnt+1].values[0] == 0:
+            X_pv = X_pv.append(row)
+            Y_pv = Y_pv.append(comb.iloc[cnt+1])
+            X_load = X_load.append(load.loc[idx])
+    cnt+=1
+    
+
+Y_load = load.loc[Y_pv.index]
+
+
+X_pv.to_csv('./processed_data/pv/X_pv.csv')
+Y_pv.to_csv('./processed_data/pv/Y_pv.csv')
+
+X_load.to_csv('./processed_data/load/X_load.csv')
+Y_load.to_csv('./processed_data/load/Y_load.csv')
